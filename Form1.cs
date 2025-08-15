7cc1af62680513d8c7adb4b21a6fdb60e683f72e
John Carl Ola <120295019+oppapi@users.noreply.github.com>
2025-08-15 22:14:04 +0800

    Finished UI

--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,15 @@ namespace Rock_Paper_Scissor
             InitializeComponent();
         }
 
+        private void randomResponse()
+        {
+            Random random = new Random();
+            int x = random.Next(1, 3);
+            if (x == 0) { pictureBox1.Image =  global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview_SCISSOR1; }
+            else if (x == 1) { pictureBox1.Image = global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview_PAPER; }
+            else { pictureBox1.Image = global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview__9_; }
+        }
+
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
@@ -29,17 +38,62 @@ namespace Rock_Paper_Scissor
 
         private void button1_Click(object sender, EventArgs e)
         {
-            pictureBox5.Image = global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview_SCISSOR1;
+            pictureBox5.Image = global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview_SCISSOR1;//rock
+            randomResponse();
+            check();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            pictureBox5.Image = global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview_PAPER;
+            pictureBox5.Image = global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview_PAPER;//paper
+            randomResponse();
+            check();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            pictureBox5.Image = global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview__9_;
+            pictureBox5.Image = global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview__9_;//scissor
+            randomResponse();
+            check();
+        }
+
+        private void check()
+        {
+            if (pictureBox5.Image == global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview_SCISSOR1
+                && pictureBox1.Image == global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview_PAPER)
+            {
+                MessageBox.Show("Computer wins!");
+            }
+            else if (pictureBox5.Image == global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview_SCISSOR1
+                     && pictureBox1.Image == global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview__9_)
+            {
+                MessageBox.Show("Player wins!");
+            }
+            else if (pictureBox5.Image == global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview_PAPER
+                     && pictureBox1.Image == global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview_SCISSOR1)
+            {
+                MessageBox.Show("Player wins!");
+            }
+            else if (pictureBox5.Image == global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview_PAPER
+                     && pictureBox1.Image == global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview__9_)
+            {
+                MessageBox.Show("Computer wins!");
+            }
+            else if (pictureBox5.Image == global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview__9_
+                     && pictureBox1.Image == global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview_SCISSOR1)
+            {
+                MessageBox.Show("Computer wins!");
+            }
+            else if (pictureBox5.Image == global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview__9_
+                     && pictureBox1.Image == global::Rock_Paper_Scissor.Properties.Resources.image_removebg_preview_PAPER)
+            {
+                MessageBox.Show("Player wins!");
+            }
+            else
+            {
+                MessageBox.Show("It's a tie!");
+            }
+
         }
     }
 }
